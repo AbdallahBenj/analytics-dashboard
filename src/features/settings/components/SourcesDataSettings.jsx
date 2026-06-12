a22aa6057f191ce52643dd93d86a424871979c89
AbdallahBenj <abdallahbenjalal@gmail.com>
2026-06-12 23:50:54 +0100

    refactor: update Data Sources size on page setting

--- src/features/settings/components/SourcesDataSettings.jsx
+++ src/features/settings/components/SourcesDataSettings.jsx
@@ -32,7 +32,7 @@ const SourcesDataSettings = () => {
     >
       <div className="max-w-4xl w-full">
         {/* Header */}
-        <div className="header-container flex flex-col md:flex-row justify-between sm:items-center gap-x-2 gap-y-6 py-2">
+        <div className="header-container flex flex-col lg:flex-row justify-between lg:items-center gap-x-2 gap-y-6 py-2">
           <div className="basis-1/3">
             <h3 className="text-xl font-bold mb-1 text-gray-700 dark:text-gray-200">
               Data Sources
@@ -53,7 +53,7 @@ const SourcesDataSettings = () => {
               state={dataSource}
               setState={setDataSource}
               stateConfig={dataSourceOptions}
-              RadioGroupClass={"w-full min-h-9 sm:min-w-36"}
+              RadioGroupClass={"w-full min-h-22 sm:min-h-9 sm:min-w-36"}
               RadioGroupOptionsClass={"w-full sm:w-[calc(50%-16px)]"}
               ButtonColorClass={`${isSupabaseData ? "bg-emerald-500" : "bg-indigo-500"}`}
             />
